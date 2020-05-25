7e17d5df65da4165cf983fc99696d1305b0c5ce1
Pisces372 <43052167+Pisces372@users.noreply.github.com>
2020-05-24 18:48:03 -0700

    ex2.py

---
+++ ex3.py
@@ -0,0 +1,34 @@
+#Statement prints I will now count my chickens
+print("I will now count my chickens:")
+
+#prints Hens and divdes 30 by 6 and adds the quotient to 25.
+print("Hens ", 25.0 + 30.0 / 6.0)
+#prints Roosters and follows the rule of PEMDAS to print 97
+print("Roosters ", 100.0 - 25.0 * 3.0 % 4.0)
+
+#prints Now I will count the eggs
+print("Now I will count the eggs:")
+
+#Literally adds up the numbers and prints the sum.
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+#Prints the statement in the quotes.
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#Prints the simplified inequality in the parantheses.
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+#Prints the statement in the quotes and then the sum of the integers.
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+#Prints the statement in the quotes.
+print("Oh, that's why it's False.")
+
+#Prints the statement in the quotes.
+print("How about some more.")
+
+#Prints the statement in the quotes then the boolean values for the inequality.
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
