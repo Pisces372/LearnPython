06520c96638e4d4dbc2bb1516b5af8f058c09c2f
Pisces372 <43052167+Pisces372@users.noreply.github.com>
2020-05-24 19:29:37 -0700

    ex6.py

---
+++ ex6.py
@@ -0,0 +1,33 @@
+#Declares variable and assigns it a value.
+types_of_people = 10
+x = f"There are {types_of_people} types of people."
+
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}."
+
+#Prints variables that have been declared earlier.
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said: '{y}'")
+
+#Declares variables
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+#Prints a variable that is formatted.
+print(joke_evaluation.format(hilarious))
+
+#Declares 2 variables
+w = "This is the left side of..."
+e = "a string with a right side."
+
+#Prints the joint of two variables.
+print(w + e)
+
+#Study Drills
+#2. Line 7, 13, 14, and 21.
+#3. Yes.
+#4. Its concatenating two strings and computer recognizes it as one string.
